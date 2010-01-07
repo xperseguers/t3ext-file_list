afcf3c05ca0980c61f125fc20f6e01bd0863e79d
Xavier Perseguers <xavier@typo3.org>
2010-01-07 12:42:22 +0000

    Added back skeleton for having EXT:indexed_search working (should now be debugged)

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,12 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_filelist_pi1.php', '_pi1', 'list_type', 0);
+
+// Hook to allow indexed search to work
+if (t3lib_extMgm::isLoaded('indexed_search')) {
+	$indexedSearchConfig = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['indexed_search']);
+	if (!$indexedSearchConfig['disableFrontendIndexing']) {
+		$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['file_list']['extraItemMarkerHook'][] = 'EXT:lb_supported/Classes/Hooks/class.user_tx_filelist_indexedsearch.php:user_tx_filelist_indexedsearch->indexExternalFile';
+	}
+}
 ?>
