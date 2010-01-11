eaf25f2d497589107807c956e7175ae9add2948e
Moreno Felscher <moreno@luagsh.ch>
2010-01-11 00:46:19 +0000

    Updated md5 hashes

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "file_list"
+# Extension Manager/Repository config file for ext "file_list".
 #
-# Auto generated 17-07-2008 00:21
+# Auto generated 10-01-2010 16:45
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'plugin',
 	'shy' => 0,
 	'version' => '1.0.0',
-	'dependencies' => '',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:40:{s:9:"ChangeLog";s:4:"1ffa";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"0522";s:17:"ext_localconf.php";s:4:"0e4f";s:14:"ext_tables.php";s:4:"94f7";s:14:"ext_tables.sql";s:4:"451a";s:13:"locallang.xml";s:4:"ea1c";s:16:"locallang_db.xml";s:4:"255b";s:14:"doc/manual.sxw";s:4:"6f6b";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_filelist_pi1.php";s:4:"fdbe";s:37:"pi1/class.tx_filelist_pi1_wizicon.php";s:4:"48b3";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"2ce0";s:17:"pi1/icons/doc.png";s:4:"3996";s:18:"pi1/icons/down.gif";s:4:"0297";s:18:"pi1/icons/draw.png";s:4:"47dc";s:19:"pi1/icons/flash.png";s:4:"35cd";s:20:"pi1/icons/folder.png";s:4:"4f81";s:27:"pi1/icons/folder_denied.png";s:4:"c2d3";s:18:"pi1/icons/html.png";s:4:"1577";s:19:"pi1/icons/image.png";s:4:"5f73";s:18:"pi1/icons/mime.png";s:4:"b0c2";s:25:"pi1/icons/mime_denied.png";s:4:"d7aa";s:21:"pi1/icons/move_up.png";s:4:"ae9b";s:17:"pi1/icons/neu.png";s:4:"ab0b";s:17:"pi1/icons/new.png";s:4:"b085";s:22:"pi1/icons/password.png";s:4:"59fb";s:17:"pi1/icons/pdf.png";s:4:"fb48";s:21:"pi1/icons/pdf_old.png";s:4:"7377";s:17:"pi1/icons/pps.png";s:4:"bf4b";s:20:"pi1/icons/reload.png";s:4:"fc7c";s:19:"pi1/icons/sound.png";s:4:"92f6";s:20:"pi1/icons/source.png";s:4:"6a30";s:17:"pi1/icons/tar.png";s:4:"e97d";s:17:"pi1/icons/txt.png";s:4:"9088";s:16:"pi1/icons/up.gif";s:4:"e951";s:19:"pi1/icons/video.png";s:4:"e014";s:17:"pi1/icons/xls.png";s:4:"bd47";s:24:"pi1/static/editorcfg.txt";s:4:"5385";}',
+	'_md5_values_when_last_written' => 'a:49:{s:9:"ChangeLog";s:4:"053b";s:20:"class.ext_update.php";s:4:"40d7";s:21:"ext_conf_template.txt";s:4:"3ce3";s:12:"ext_icon.gif";s:4:"0522";s:17:"ext_localconf.php";s:4:"35eb";s:14:"ext_tables.php";s:4:"bc55";s:40:"Configuration/FlexForms/flexform_pi1.xml";s:4:"69bf";s:57:"Configuration/FlexForms/flexform_pi1_rgfolderselector.xml";s:4:"6cf7";s:34:"Configuration/TypoScript/setup.txt";s:4:"532d";s:40:"Resources/Private/Language/locallang.xml";s:4:"605b";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"149d";s:44:"Resources/Private/Language/locallang_pi1.xml";s:4:"222b";s:45:"Resources/Private/Templates/template_pi1.html";s:4:"b48f";s:47:"Resources/Public/Icons/Files/blank_document.png";s:4:"b0c2";s:49:"Resources/Public/Icons/Files/category_archive.png";s:4:"e97d";s:50:"Resources/Public/Icons/Files/category_document.png";s:4:"b0c2";s:47:"Resources/Public/Icons/Files/category_flash.png";s:4:"35cd";s:47:"Resources/Public/Icons/Files/category_image.png";s:4:"5f73";s:47:"Resources/Public/Icons/Files/category_sound.png";s:4:"92f6";s:48:"Resources/Public/Icons/Files/category_source.png";s:4:"6a30";s:47:"Resources/Public/Icons/Files/category_video.png";s:4:"e014";s:36:"Resources/Public/Icons/Files/doc.png";s:4:"3996";s:37:"Resources/Public/Icons/Files/draw.png";s:4:"47dc";s:37:"Resources/Public/Icons/Files/html.png";s:4:"1577";s:36:"Resources/Public/Icons/Files/pdf.png";s:4:"fb48";s:36:"Resources/Public/Icons/Files/pps.png";s:4:"bf4b";s:36:"Resources/Public/Icons/Files/txt.png";s:4:"9088";s:36:"Resources/Public/Icons/Files/xls.png";s:4:"bd47";s:41:"Resources/Public/Icons/Folders/folder.png";s:4:"4f81";s:42:"Resources/Public/Icons/Folders/move_up.png";s:4:"ae9b";s:38:"Resources/Public/Icons/Sorting/asc.gif";s:4:"16a2";s:39:"Resources/Public/Icons/Sorting/desc.gif";s:4:"e0be";s:22:"Samples/gallery/README";s:4:"84e9";s:25:"Samples/gallery/setup.txt";s:4:"ceee";s:29:"Samples/gallery/template.html";s:4:"33ed";s:39:"Samples/gallery/tx_filelist_gallery.php";s:4:"7ddb";s:23:"Samples/metadata/README";s:4:"f9c7";s:26:"Samples/metadata/setup.txt";s:4:"4cc8";s:30:"Samples/metadata/template.html";s:4:"060c";s:41:"Samples/metadata/tx_filelist_metadata.php";s:4:"c674";s:27:"Samples/multilingual/README";s:4:"fd47";s:30:"Samples/multilingual/setup.txt";s:4:"8a4a";s:34:"Samples/multilingual/template.html";s:4:"8d22";s:49:"Samples/multilingual/tx_filelist_multilingual.php";s:4:"9eec";s:14:"doc/manual.sxw";s:4:"1e8a";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:32:"pi1/class.tx_filelist_helper.php";s:4:"6279";s:29:"pi1/class.tx_filelist_pi1.php";s:4:"4df3";s:37:"pi1/class.tx_filelist_pi1_wizicon.php";s:4:"c6ad";}',
+	'suggests' => array(
+	),
 );
 
 ?>
