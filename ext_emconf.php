4b39b3e3f4c56f34b93908dc73513c18d7a2db71
Xavier Perseguers <xavier@causal.ch>
2016-01-04 13:26:16 +0100

    [!!!][TASK] Raise dependency range
    - PHP 5.5 is now mandatory
    - Support for TYPO3 8
    
    Change-Id: I8068a6a98168c9238f6d23507c283784b7d691e8
    Reviewed-on: https://review.typo3.org/45563
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,40 +11,35 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'File List',
-	'description' => 'This extension provides a frontend plugin which shows a list of files and folders in a specified directory on the file system (comparable to Apache\'s directory listing).',
-	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '2.0.0-dev',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
-	'author' => 'Xavier Perseguers and Moreno Feltscher',
-	'author_email' => 'xavier@causal.ch',
-	'author_company' => 'Causal Sàrl',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
-			'php' => '5.3.3-5.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+    'title' => 'File List',
+    'description' => 'This extension provides a frontend plugin which shows a list of files and folders in a specified directory on the file system (comparable to Apache\'s directory listing).',
+    'category' => 'plugin',
+    'shy' => 0,
+    'version' => '2.0.0-dev',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Xavier Perseguers and Moreno Feltscher',
+    'author_email' => 'xavier@causal.ch',
+    'author_company' => 'Causal Sàrl',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-8.99.99',
+            'php' => '5.5.0-7.0.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+    'suggests' => array(),
 );
-
-?>
