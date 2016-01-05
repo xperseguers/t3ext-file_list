0f189f33162e23489db5de3e09e6341760fea5d9
Xavier Perseguers <xavier@causal.ch>
2016-01-05 17:07:42 +0100

    [TASK] Anonymous function to initialize the extension
    This is inspired by EXT:news.
    
    Change-Id: Id74da1ced7a561a5911c522b2b2453423024494d
    Reviewed-on: https://review.typo3.org/45593
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,25 +1,33 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Controller/Pi1/Pi1Controller.php', '_pi1', 'list_type', $settings['noCache'] ? 0 : 1);
+$boot = function ($_EXTKEY) {
+    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Controller/Pi1/Pi1Controller.php', '_pi1', 'list_type', $settings['noCache'] ? 0 : 1);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Causal.' . $_EXTKEY,
-    'Filelist',
-    array(
-        'File' => 'list',
-    ),
-    // non-cacheable actions
-    []
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'Causal.' . $_EXTKEY,
+        'Filelist',
+        // cacheable actions
+        [
+            'File' => 'list',
+        ],
+        // non-cacheable actions
+        []
+    );
 
-// Register default template layouts
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['file_list']['templateLayouts'][] = [
-    'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist.templateLayout.simple',
-    'Simple'
-];
-$GLOBALS['TYPO3_CONF_VARS']['EXT']['file_list']['templateLayouts'][] = [
-    'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist.templateLayout.thumbnailDescription',
-    'ThumbnailDescription'
-];
+    /* ===========================================================================
+        Register default template layouts
+    =========================================================================== */
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['file_list']['templateLayouts'][] = [
+        'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist.templateLayout.simple',
+        'Simple'
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['file_list']['templateLayouts'][] = [
+        'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist.templateLayout.thumbnailDescription',
+        'ThumbnailDescription'
+    ];
+};
+
+$boot($_EXTKEY);
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -1,32 +1,37 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-// Disable the display of layout, select_key and page fields
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
+$boot = function ($_EXTKEY) {
+    // Disable the display of layout, select_key and page fields
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
 
-// Activate the display of the plug-in flexform field and set FlexForm definition
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+    // Activate the display of the plug-in flexform field and set FlexForm definition
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:file_list/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:file_list/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY . '_pi1'), 'list_type');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'Causal.' . $_EXTKEY,
-    'Filelist',
-    'File List - List of files'
-);
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        'Causal.' . $_EXTKEY,
+        'Filelist',
+        'File List - List of files'
+    );
 
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
-$pluginSignature = strtolower($extensionName) . '_filelist';
-// Disable the display of layout, select_key and page fields
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+    $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+    $pluginSignature = strtolower($extensionName) . '_filelist';
+    // Disable the display of layout, select_key and page fields
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-if (version_compare(TYPO3_version, '7.6', '>=')) {
-    $flexform = 'flexform_filelist.xml';
-} else {
-    $flexform = 'flexform_filelist_62.xml';
-}
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/' . $flexform);
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+    if (version_compare(TYPO3_version, '7.6', '>=')) {
+        $flexform = 'flexform_filelist.xml';
+    } else {
+        $flexform = 'flexform_filelist_62.xml';
+    }
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/' . $flexform);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File List');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File List');
+};
+
+$boot($_EXTKEY);
+unset($boot);
