4f753bb6ffb73f4f40160ebe2d845131394edef7
Xavier Perseguers <xavier@causal.ch>
2016-01-05 18:18:02 +0100

    [TASK] Allow legacy plugin to be disabled in Extension Manager
    Change-Id: I0246e39e42c4de3c4e5708f77e080920174585fb
    Reviewed-on: https://review.typo3.org/45595
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,23 @@ defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
     $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'Classes/Controller/Pi1/Pi1Controller.php', '_pi1', 'list_type', $settings['noCache'] ? 0 : 1);
 
+    /* ===========================================================================
+        Legacy plugin (pibase-based)
+    =========================================================================== */
+    if (!isset($settings['enableLegacyPlugin']) || (bool)$settings['enableLegacyPlugin']) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
+            $_EXTKEY,
+            'Classes/Controller/Pi1/Pi1Controller.php',
+            '_pi1',
+            'list_type',
+            $settings['noCache'] ? 0 : 1
+        );
+    }
+
+    /* ===========================================================================
+        Extbase-based plugin
+    =========================================================================== */
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'Causal.' . $_EXTKEY,
         'Filelist',
@@ -16,7 +31,9 @@ $boot = function ($_EXTKEY) {
         []
     );
 
-    // Page module hook
+    /* ===========================================================================
+        Page module hook
+    =========================================================================== */
     $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
     $pluginSignature = strtolower($extensionName) . '_filelist';
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info'][$pluginSignature][$_EXTKEY] =
--- ext_tables.php
+++ ext_tables.php
@@ -2,21 +2,33 @@
 defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
-    // Disable the display of layout, select_key and page fields
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages,recursive';
+    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
-    // Activate the display of the plug-in flexform field and set FlexForm definition
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+    /* ===========================================================================
+        Legacy plugin (pibase-based)
+    =========================================================================== */
+    if (!isset($settings['enableLegacyPlugin']) || (bool)$settings['enableLegacyPlugin']) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+            [
+                'LLL:EXT:file_list/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi1',
+                $_EXTKEY . '_pi1'
+            ],
+            'list_type'
+        );
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-        [
-            'LLL:EXT:file_list/Resources/Private/Language/locallang_db.xml:tt_content.list_type_pi1',
-            $_EXTKEY . '_pi1'
-        ],
-        'list_type'
-    );
+        $pluginSignature = $_EXTKEY . '_pi1';
 
+        // Disable the display of layout, select_key and page fields
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+
+        // Activate the display of the plugin FlexForm field and set FlexForm definition
+        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_pi1.xml');
+    }
+
+    /* ===========================================================================
+        Extbase-based plugin
+    =========================================================================== */
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
         'Causal.' . $_EXTKEY,
         'Filelist',
@@ -25,9 +37,11 @@ $boot = function ($_EXTKEY) {
 
     $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
     $pluginSignature = strtolower($extensionName) . '_filelist';
+
     // Disable the display of layout, select_key and page fields
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
 
+    // Activate the display of the plugin FlexForm field and set FlexForm definition
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
     if (version_compare(TYPO3_version, '7.6', '>=')) {
         $flexform = 'flexform_filelist.xml';
@@ -36,6 +50,9 @@ $boot = function ($_EXTKEY) {
     }
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/' . $flexform);
 
+    /* ===========================================================================
+        Register default TypoScript
+    =========================================================================== */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File List');
 };
 
