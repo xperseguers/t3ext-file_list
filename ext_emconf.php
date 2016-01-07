d18c2cd1daa9f93be559d786d3403b4cdfc9bb99
Xavier Perseguers <xavier@causal.ch>
2016-01-07 20:51:44 +0100

    [BUGFIX] PathUtility::getAbsoluteWebPath() does not exist
    Before TYPO3 6.2.16.
    
    Change-Id: Ic68661f360e7325812f1abbe3701d0036ff4e277
    Resolves: #72575
    Reviewed-on: https://review.typo3.org/45660
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.16-7.99.99',
             'php' => '5.5.0-7.0.99',
         ),
         'conflicts' => array(),
