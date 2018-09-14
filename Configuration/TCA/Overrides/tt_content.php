271b356fc78d377026d64adc715ed17dee824ef6
Xavier Perseguers <xavier@causal.ch>
2018-09-14 13:51:47 +0200

    [TASK] Move declaration of plugin out of ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,18 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+// Register Frontend plugin
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Causal.file_list',
+    'Filelist',
+    'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist_title'
+);
+
+$pluginSignature = 'filelist_filelist';
+
+// Disable the display of layout, select_key and page fields
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+
+// Activate the display of the plugin FlexForm field and set FlexForm definition
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:file_list/Configuration/FlexForms/flexform_filelist.xml');
--- ext_tables.php
+++ ext_tables.php
@@ -2,25 +2,6 @@
 defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
-    /* ===========================================================================
-        Extbase-based plugin
-    =========================================================================== */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'Causal.' . $_EXTKEY,
-        'Filelist',
-        'LLL:EXT:file_list/Resources/Private/Language/locallang_flexform.xlf:filelist_title'
-    );
-
-    $extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
-    $pluginSignature = strtolower($extensionName) . '_filelist';
-
-    // Disable the display of layout, select_key and page fields
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
-
-    // Activate the display of the plugin FlexForm field and set FlexForm definition
-    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_filelist.xml');
-
     // Register the wizard for new content element
     $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses'][\Causal\FileList\Controller\FileControllerWizard::class] =
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/FileControllerWizard.php';
