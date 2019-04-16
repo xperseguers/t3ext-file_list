44a9b9655f6fc03e4df335e18009e781fa3a12a2
Xavier Perseguers <xavier@causal.ch>
2019-04-16 16:49:52 +0200

    [TASK] Migrate registration of static template

--- ext_tables.php
+++ ext_tables.php
@@ -5,12 +5,6 @@ $boot = function ($_EXTKEY) {
     // Register the wizard for new content element
     $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses'][\Causal\FileList\Controller\FileControllerWizard::class] =
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/FileControllerWizard.php';
-
-    /* ===========================================================================
-        Register default TypoScript
-    =========================================================================== */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'File List');
-
 };
 
 $boot('file_list');
