c229fc5692f76e9b76761390cfa2f23ce3d07bc1
Xavier Perseguers <xavier@causal.ch>
2019-11-14 10:33:53 +0100

    [BUGFIX] Prevent error while checking ext_tables.php in TYPO3 v9 Upgrade Wizard

--- ext_tables.php
+++ ext_tables.php
@@ -2,9 +2,12 @@
 defined('TYPO3_MODE') || die();
 
 $boot = function ($_EXTKEY) {
-    // Register the wizard for new content element
-    $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses'][\Causal\FileList\Controller\FileControllerWizard::class] =
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/FileControllerWizard.php';
+    if (version_compare(TYPO3_branch, '9.5', '<')) {
+        // Register the wizard for new content element
+        // TODO: For TYPO3 v9, see https://docs.typo3.org/m/typo3/reference-coreapi/9.5/en-us/ApiOverview/Examples/ContentElementWizard/Index.html
+        $GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses'][\Causal\FileList\Controller\FileControllerWizard::class] =
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Controller/FileControllerWizard.php';
+    }
 };
 
 $boot('file_list');
