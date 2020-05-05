bdca1a22daa807ab576199815ef2dab502510788
Xavier Perseguers <xavier@causal.ch>
2020-05-05 10:35:37 +0200

    [TASK] Adapt dependency range for TYPO3 v10 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.3.99',
+            'typo3' => '8.7.0-10.4.99',
             'php' => '7.2.0-7.4.99',
         ],
         'conflicts' => [],
