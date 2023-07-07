c750ab65f874ec1c0495e4df6be1ec459de99b2f
Xavier Perseguers <xavier@causal.ch>
2023-07-07 08:32:49 +0200

    [TASK] Fix obvious problems to make it run with TYPO3 v12

--- Classes/Controller/FileController.php
+++ Classes/Controller/FileController.php
@@ -190,6 +190,13 @@ class FileController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
             'newTimestamp' => $newTimestamp,
             'data' => $this->configurationManager->getContentObject()->data,
         ]);
+
+        $typo3Branch = class_exists(\TYPO3\CMS\Core\Information\Typo3Version::class)
+            ? (new \TYPO3\CMS\Core\Information\Typo3Version())->getBranch()
+            : TYPO3_branch;
+        if (version_compare($typo3Branch, '11.5', '>=')) {
+            return $this->htmlResponse();
+        }
     }
 
     /**
