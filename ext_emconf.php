41be5dc4098c7ac2250fbb5289fd7e9dfd3b63af
Xavier Perseguers <xavier@causal.ch>
2024-02-26 17:18:45 +0100

    [TASK] Kickstart compatibility with TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'File List',
     'description' => 'This extension provides a frontend plugin which shows a list of files and folders in a specified directory on the file system (comparable to Apache directory listing) or using more advanced FAL selectors (categories, collection of files, ...). This extension may also be used for creating image galleries. Default templates are Bootstrap-ready.',
     'category' => 'plugin',
-    'version' => '3.0.1',
+    'version' => '3.1.0-dev',
     'state' => 'stable',
     'author' => 'Xavier Perseguers',
     'author_email' => 'xavier@causal.ch',
     'author_company' => 'Causal Sàrl',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-12.4.99',
+            'typo3' => '10.4.0-13.0.99',
             'php' => '7.4.0-8.3.99',
         ],
         'conflicts' => [],
