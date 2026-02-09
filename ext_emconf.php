ad8d9359c944814d27245e0ba924db2a0f7817e3
Xavier Perseguers <xavier@causal.ch>
2026-02-09 20:18:07 +0100

    [TASK] Raise compatibility for TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Causal Sàrl',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-14.0.99',
+            'typo3' => '11.5.0-14.3.99',
             'php' => '7.4.0-8.5.99',
         ],
         'conflicts' => [],
